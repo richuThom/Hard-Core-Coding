b2a800cc205bf656a83b0935cf33109a1d108d54
richuThom <richu.thomas2206@gmail.com>
2024-05-21 12:09:38 +1200

    basic index file

---
+++ App.js
@@ -0,0 +1,26 @@
+/**
+ * <div id="parent">
+ *  <div id="child1">
+ *  <h1>Im an h1 tag</h1>
+ *  <h2>Im an h2 tag</h2>
+ *  </div>
+ * <div id="child2">
+ *  <h1>Im an h1 tag</h1>
+ *  <h2>Im an h2 tag</h2>
+ *  </div>
+ * </div>
+ * 
+ */
+const parent = React.createElement("div",{id:"parent"},
+React.createElement("div",{id:"child1"},
+[React.createElement("h1",{},"Im an h1 tag"),React.createElement("h2",{},"Im an h2 tag")]
+),
+React.createElement("div",{id:"child2"},
+[React.createElement("h1",{},"Im an h1 tag"),React.createElement("h2",{},"Im an h2 tag")]
+)
+);
+const heading =React.createElement("h1",{id : "abc"},"Hello World from REACT!");
+console.log(parent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+//console.log(root);
+root.render(parent);
